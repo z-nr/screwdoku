e50076c403626235f327648c2e7ad63dd1252026
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-21 14:44:36 -0700

    Switch method names for get_pos and get_val

--- sudoku.rb
+++ sudoku.rb
@@ -10,7 +10,7 @@ class SudokuGame
     @board = board
   end
 
-  def get_pos
+  def get_val
     pos = nil
     until pos && valid_val?(pos)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
@@ -20,7 +20,7 @@ class SudokuGame
     pos
   end
 
-  def get_val
+  def get_pos
     val = nil
     until val && valid_pos?(val)
       puts "Please enter a position on the board (e.g., '3,4')"
@@ -49,8 +49,9 @@ class SudokuGame
   def play_turn
     board.render
 
-    val = get_val
     pos = get_pos
+    val = get_val
+
 
     board[pos] = val
   end
