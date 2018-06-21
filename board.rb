451f2dd3d21a99adc2da75e8f50400690851836f
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-21 15:23:04 -0700

    Delete unnecessary end and remove evil laughter

--- board.rb
+++ board.rb
@@ -53,9 +53,8 @@ class Board
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
-      columns.all? { |col| solved_set?(col) } &&
-      squares.all? { |square| solved_set?(square) }
-    end
+    columns.all? { |col| solved_set?(col) } &&
+    squares.all? { |square| solved_set?(square) }
   end
 
   def solved_set?(tiles)
